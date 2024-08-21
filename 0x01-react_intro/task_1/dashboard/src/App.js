cc6c51f5540860c4ef39347b6b38b3f40d29fb1b
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2024-08-21 20:41:52 +0200

    modify notifications

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "./App.css";
 import { getFullYear, getFooterCopy } from "./utils";
+import holbertonLogo from "./Holberton-logo.jpg";
 
 function App() {
   const isIndexPage = true;
+
+  // Function to handle click event on labels to focus the corresponding input
+  // eslint-disable-next-line
+  const handleClick = (e) => {
+    e.target.nextElementSibling.focus(); // Focuses the next sibling element, which is the input field
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img
-          src="task_0/dashboard/src/Holberton_logo.jpg"
+          src={holbertonLogo}
           alt="Holberton Logo"
+          width={100}
+          height={100}
         />
         <h1>School Dashboard</h1>
       </header>
       <body className="App-body">
         <hr />
         <p>Login to access the full dashboard</p>
+        <form className="form-horizontal">
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit">OK</button>
+        </form>
         <hr />
       </body>
       <footer className="App-footer">
