a2b91c045cbf05c08d6f624cf32bce2818e60f75
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2024-08-29 15:55:35 +0200

    create pure notifications components + test

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import "./Notifications.css";
+
+export default class NotificationItem extends PureComponent {
+  render() {
+    const { type, html, value, id, markAsRead } = this.props;
+    return (
+      <li
+        data-notification-type={type}
+        onClick={() => markAsRead(id)}
+        dangerouslySetInnerHTML={html ? html : undefined}
+      >
+        {html ? null : value}
+      </li>
+    );
+  }
+}
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string.isRequired,
+  }),
+  value: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  markAsRead: PropTypes.func.isRequired,
+};
+
+NotificationItem.defaultProps = {
+  type: "default",
+  markAsRead: () => {},
+};
