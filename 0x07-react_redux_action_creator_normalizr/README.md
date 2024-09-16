5bee702b87db09b3f51cbe26f0f6560d24eefdeb
KeaMohlala <keamogetsemhll@gmail.com>
2024-09-16 11:38:44 +0000

    move bashboard from last project

---
+++ 0x07-react_redux_action_creator_normalizr/README.md
@@ -0,0 +1,10 @@
+#ALX SE Specializatins front-end #0x07-react_redux_action_creator_normalizr
+
+
+- Normalizr’s purpose and how to use it
+- schemas and normalization of nested JSON
+- core concepts of Redux
+- Redux actions
+- Redux action creators
+- async actions in Redux
+- how to write tests for Redux
---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/CourseList/CourseList.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+import CourseList from "./CourseList";
+
+describe("CourseList component", () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  it("renders without crashing", () => {
+    shallow(<CourseList />);
+  });
+
+  it("renders correctly with an empty listCourses prop", () => {
+    const wrapper = shallow(<CourseList listCourses={[]} />);
+    expect(wrapper.find("CourseListRow").length).toEqual(3); // Includes the header rows
+    expect(wrapper.find("CourseListRow").at(2).prop("textFirstCell")).toEqual(
+      "No course available yet"
+    );
+  });
+
+  it("renders correctly with a list of courses", () => {
+    const listCourses = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+    ];
+    const wrapper = shallow(<CourseList listCourses={listCourses} />);
+    expect(wrapper.find("CourseListRow")).toHaveLength(4); // 2 header rows + 2 courses
+  });
+});
